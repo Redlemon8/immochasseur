94b4f4bc2b615a855b1e4a77fb27cd87f290c471
redlemon8 <hanquezbenjamin@gmx.com>
2024-06-22 10:24:08 +0200

    add alert form message

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let lastScrollTop = 0;
 const banner = document.getElementById('banner');
+const form = document.getElementById('myForm');
 
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYoffset || this.document.documentElement.scrollTop;
@@ -14,4 +15,9 @@ window.addEventListener('scroll', function() {
         banner.style.opacity = '1';
     }
     lastScrollTop = scrollTop;
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Votre message a été envoyé avec succés !');
 });
